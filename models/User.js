aac663f80bbad9805519fd8a500e550e9928b971
Varun Deva <varundev23@gmail.com>
2021-12-04 22:31:16 +0530

    fix: added strict- false

--- models/User.js
+++ models/User.js
@@ -7,26 +7,30 @@ const userSchema = new mongoose.Schema(
       unique: true,
     },
     name: String,
-    results: {
-      registrationNumber: String,
-      studentName: String,
-      degree: String,
-      course: String,
-      college: String,
-      examinationsOf: String,
-      markSheet: [
-        {
-          semester: String,
-          subjectName: String,
-          subjectCode: String,
-          component: String,
-          marks: String,
-        },
-      ],
-    },
+    results: [
+      {
+        registrationNumber: String,
+        studentName: String,
+        degree: String,
+        course: String,
+        college: String,
+        examinationsOf: String,
+        pdfDownloadUrl: String,
+        markSheet: [
+          {
+            semester: String,
+            subjectName: String,
+            subjectCode: String,
+            component: String,
+            marks: String,
+          },
+        ],
+      },
+    ],
   },
   {
     timestamps: true,
+    strict: false,
   }
 );
 
