b286d4a2a6a422c87a3529c416d4f4aa42046ab1
Varun Ganesh D <varun.d1@byjus.com>
2021-12-04 18:40:26 +0530

    fix: download path issue

--- scraper/rawResultScraper.js
+++ scraper/rawResultScraper.js
@@ -45,7 +45,8 @@ const savePdf = async (registerNumber) => {
 
     await page.waitForSelector(".table");
     const fileName = `${registerNumber}.pdf`;
-    const savePath = `../pdfs/${fileName}`;
+    const savePath = `pdfs/${fileName}`;
+
     await page.pdf({
       path: savePath, // Saves pdf to disk.
       format: "A4",
